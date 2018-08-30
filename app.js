b282ec385ed159dc17d025fe3f02cc6c77be2648
akinhwan <akinhwan@gmail.com>
2018-08-29 21:08:58 -0400

    add app.js and img

---
+++ app.js
@@ -0,0 +1,67 @@
+let r1 = document.querySelector(".r1");
+let r2 = document.querySelector(".r2");
+let r3 = document.querySelector(".r3");
+let r4 = document.querySelector(".r4");
+let r5 = document.querySelector(".r5");
+
+let a1 = document.querySelector(".a1");
+let a2 = document.querySelector(".a2");
+let a3 = document.querySelector(".a3");
+let a4 = document.querySelector(".a4");
+let a5 = document.querySelector(".a5");
+
+let d1 = document.querySelector(".d1");
+let d2 = document.querySelector(".d2");
+let d3 = document.querySelector(".d3");
+let d4 = document.querySelector(".d4");
+let d5 = document.querySelector(".d5");
+
+a1.addEventListener("click", () => {
+  r1.style.opacity = 0;
+  setTimeout(() => (r1.style.display = "none"), 500);
+});
+
+a2.addEventListener("click", () => {
+  r2.style.opacity = 0;
+  setTimeout(() => (r2.style.display = "none"), 500);
+});
+
+a3.addEventListener("click", () => {
+  r3.style.opacity = 0;
+  setTimeout(() => (r3.style.display = "none"), 500);
+});
+
+a4.addEventListener("click", () => {
+  r4.style.opacity = 0;
+  setTimeout(() => (r4.style.display = "none"), 500);
+});
+
+a5.addEventListener("click", () => {
+  r5.style.opacity = 0;
+  setTimeout(() => (r15.style.display = "none"), 500);
+});
+
+d1.addEventListener("click", () => {
+  r1.style.opacity = 0;
+  setTimeout(() => (r1.style.display = "none"), 500);
+});
+
+d2.addEventListener("click", () => {
+  r2.style.opacity = 0;
+  setTimeout(() => (r2.style.display = "none"), 500);
+});
+
+d3.addEventListener("click", () => {
+  r3.style.opacity = 0;
+  setTimeout(() => (r3.style.display = "none"), 500);
+});
+
+d4.addEventListener("click", () => {
+  r4.style.opacity = 0;
+  setTimeout(() => (r4.style.display = "none"), 500);
+});
+
+d5.addEventListener("click", () => {
+  r5.style.opacity = 0;
+  setTimeout(() => (r5.style.display = "none"), 500);
+});
